0dd875d288e107df5184312ffa00998f74d704e9
ThierryNathan <thierrynathan1@gmail.com>
2024-03-29 18:00:54 -0300

    Update JS calc

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,47 @@
-//Tratamento de eventos
-//document.querySelector("#modelo-tenis").addEventListener("change", calcular)
-//document.querySelector("tamanho"),addEventListener("input", function () 
-//    {
-//        const tamanho = document.querySelector("tamanho").value
-//        document.querySelector("label[for=tamanho]").innerText = "Tamanho " + tamanho;
-//        calcular();
-//    })
-//document.querySelector("mod-sim").addEventListener("change", calcular)
-//document.querySelector("mod-nao").addEventListener("change", calcular)
+// Tratamento de eventos
 
-function calcular()
+document.querySelector("#modelo-tenis").addEventListener("change", calcular);
+document.querySelector("#tamanho").addEventListener("input", exibirTamanho);
+
+document.querySelectorAll("input[name='cor']").forEach(cor => {
+    cor.addEventListener("change", calcular);
+});
+
+function exibirTamanho() 
 {
-    // DOM - Document Object Model
-    let modeloTenis = document.querySelector("#modelo-tenis").value;
-    valorTenis = 0;
-    console.log(modeloTenis);
-
-    //const modificar = document.querySelector("mod-tenis").checked adicionar mod tenis para calcular e modificar o valor
-
-    switch (valorTenis) 
-    {
-        case modeloTenis: 1
-            valorTenis = 788.49
-            break;
-        case modeloTenis: 2
-            valorTenis = 1139.99
-            break;
-        case modeloTenis: 3
-            valorTenis = 949.99
-            break;
-        default:
-            break;
-    }
-
-    
-    console.log(valorTenis);
-    document.querySelector("#valorTenis").innerText = "R$: " + valorTenis.toFixed(2);
+    const tamanhoSelecionado = document.querySelector("#tamanho").value;
+    document.querySelector("#tamanhoSelecionado").innerText = tamanhoSelecionado;
 }
 
+function calcular() 
+{
+    // DOM - Document Object Model
+    const modeloTenis = document.querySelector("#modelo-tenis").value;
+    const valoresModelo = {"1": 788.49, "2": 1139.99, "3": 949.99};
+    const valorTenisBase = valoresModelo[modeloTenis] || 0;
 
+    const valoresAdicionaisPorCor = 
+    {
+        "azul": 20,
+        "branco": 15,
+        "vermelho": 25
+    };
 
+    let valorTenisFinal = valorTenisBase;
+
+    const cores = document.querySelectorAll("input[name='cor']");
+    cores.forEach(cor => 
+    {
+        if (cor.checked) 
+        {
+            const corSelecionada = cor.value;
+            const valorAdicional = valoresAdicionaisPorCor[corSelecionada] || 0;
+            valorTenisFinal += valorAdicional;
+        }
+    });
+
+    console.log(valorTenisFinal);
+    document.querySelector("#valorTenis").innerText = "Valor total R$: " + valorTenisFinal.toFixed(2);
+}
+
+exibirTamanho();
